7d85da46250efb285a6d68c8003d4a29bb0ba21d
amin <amingholipoor5327@gamil.com>
2026-06-12 18:14:09 +0330

    added css for main page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,19 @@
 import Link from "next/link";
+import styles from "./home.module.css";
+
 export default function Home() {
   return (
-    <div>
-      <main>
- <h1>Hi!</h1>
-    <Link href={"/api/products"}> go to see products </Link>
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Hi! 👋</h1>
+        <div className={styles.buttonGroup}>
+          <Link href={"/api/products"} className={styles.link}>
+            📦 go to see products
+          </Link>
+          <Link href={"/data"} className={styles.link}>
+            📊 go to see data
+          </Link>
+        </div>
       </main>
     </div>
   );
